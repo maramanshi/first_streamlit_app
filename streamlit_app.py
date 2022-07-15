45b6d7d809f289bddd7703ba3437149bc20c8af8
maraman <109370060+maramanshi@users.noreply.github.com>
2022-07-15 12:51:40 -0500

    Update streamlit_app.py
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,11 @@ import streamlit
 
 streamlit.title('My Project chap1')
 
-streamlit.header('BreakFast Menu')
-streamlit.text('Blue berry muffin')
-streamlit.text('Blue berry cake')
+streamlit.header('🥣 🥗 🐔 🥑🍞 Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
