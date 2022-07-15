0aacc8f9e22c2aeaf775e9aebbb10280b3c669c3
maraman <109370060+maramanshi@users.noreply.github.com>
2022-07-15 12:56:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,10 @@ streamlit.title('My Project chap1')
 
 streamlit.header('🥣 🥗 🐔 🥑🍞 Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie 🫀')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-streamlit.header('🫀')
 
