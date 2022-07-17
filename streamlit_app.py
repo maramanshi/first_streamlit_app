b07fb83a571ee21d4ab74d91bd9ed243b8d40375
maraman <109370060+maramanshi@users.noreply.github.com>
2022-07-17 16:41:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,4 +52,4 @@ streamlit.dataframe(my_data_row)
 
 #Add a second select box
 add_my_fruit = streamlit.text_input('What fruit would you like information about?', 'jackfruit')
-streamlit.write('The user entered', add_my_fruit)
+streamlit.write('Thanks for adding', add_my_fruit)
